b2dc0afe506a9e4a2820d5e8d3738d4646e5ab0a
newyorkdeep <mpp14021972@gmail.com>
2025-08-06 23:36:24 +0300

    implementing styling

--- django_image_resizer/settings.py
+++ django_image_resizer/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Media files (Uploaded images)
 MEDIA_URL = '/media/'

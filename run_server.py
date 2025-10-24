89ada6cf528ab2111c03654c57b2e14f059d868d
newyorkdeep <mpp14021972@gmail.com>
2025-10-24 22:52:55 +0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,14 +1,19 @@
+# start_server.py
 import os
 import sys
 from django.core.management import execute_from_command_line
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_image_resizer.settings') # Replace 'your_project_name'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_image_resizer.settings')
     try:
-        execute_from_command_line(sys.argv + ['runserver', '--noreload']) # --noreload is crucial for PyInstaller
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # You might want to add a way to open the browser automatically
+    # import webbrowser
+    # webbrowser.open("http://127.0.0.1:8000")
